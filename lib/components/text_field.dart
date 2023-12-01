bd75c2b892f3ed9fc79b4921e9496e819ef22624
Wil-alf <149569448+Wil-alf@users.noreply.github.com>
2023-12-01 18:48:56 +0700

    changes to the components
    updet bang

--- lib/components/text_field.dart
+++ lib/components/text_field.dart
@@ -16,18 +16,25 @@ class MyTextField extends StatelessWidget {
     return TextField(
       controller: controller,
       obscureText: obscureText,
-      style: TextStyle(color: Color(0xFFECB365)),
+      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+      // style: TextStyle(color: Color(0xFFECB365)),
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(15)),
-            borderSide: BorderSide(color: Color(0xFF041C32), width: 2)),
+            borderSide:
+                BorderSide(color: Color.fromARGB(255, 0, 0, 0), width: 2)),
+        // borderSide: BorderSide(color: Color(0xFF041C32), width: 2)),
         focusedBorder: const OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(15)),
-            borderSide: BorderSide(color: Color(0xFFECB365), width: 2)),
-        fillColor: Color(0xFF064663),
+            borderSide:
+                BorderSide(color: Color.fromARGB(255, 0, 0, 0), width: 2)),
+        // borderSide: BorderSide(color: Color(0xFFECB365), width: 2)),
+        fillColor: Color.fromARGB(255, 255, 128, 0),
+        // fillColor: Color(0xFF064663),
         filled: true,
         hintText: hintText,
-        hintStyle: const TextStyle(color: Color(0xFFECB365)),
+        hintStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+        // hintStyle: const TextStyle(color: Color(0xFFECB365)),
       ),
     );
   }
