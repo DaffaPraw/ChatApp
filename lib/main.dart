01cfeda3ba0221b55c3ee645429c3cd9e3c1ba2c
DaffaPraw <144682172+DaffaPraw@users.noreply.github.com>
2023-12-02 00:00:00 +0700

    Add files via upload
    user pfp main.dart adjustment

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
-import 'package:chat_app/pages/register_page.dart';
-import 'package:chat_app/services/login_register.dart';
-import 'package:chat_app/setting/models/setting.dart';
 import 'package:chat_app/setting/screen/inner_setting/datausage.dart';
 import 'package:chat_app/setting/screen/inner_setting/notification.dart';
 import 'package:flutter/material.dart';
-import 'package:chat_app/setting/screen/setting_screen.dart';
 import 'package:chat_app/setting/screen/inner_setting/personal.dart';
-import 'package:chat_app/setting/screen/inner_setting/notification.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/services/auth/auth_gate.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -37,10 +31,8 @@ class MyApp extends StatelessWidget {
         '/datausage': (context) => const DataUsages(),
         '/logout': (context) => const AuthGate(),
       },
-      
-
       debugShowCheckedModeBanner: false,
-      home: SettingsScreen(),//AuthGate(),
+      home: AuthGate(),
     );
   }
 }
