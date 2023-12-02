d6f964c9cf96966744b0c0debed9c69136984e4c
imstrong07 <133095926+imstrong07@users.noreply.github.com>
2023-12-02 20:19:23 +0700

    Add files via upload
    update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/profile.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/image_service.dart';
 import 'package:chat_app/setting/screen/setting_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,6 +17,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final image_service _image_service = image_service();
 
   void signOut() {
     final authService = Provider.of<AuthService>(context, listen: false);
@@ -25,7 +27,7 @@ class _HomePageState extends State<HomePage> {
   void settings() {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const SettingsScreen()), //Profile()
+      MaterialPageRoute(builder: (context) => const SettingsScreen()),
     );
   }
 
@@ -63,26 +65,48 @@ class _HomePageState extends State<HomePage> {
 
         return ListView(
           children: snapshot.data!.docs
-              .map<Widget>((doc) => _buildUserListItem(doc))
+              .map<Widget>((doc) => FutureBuilder<Widget>(
+                    future: _buildUserListItem(doc),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return snapshot.data!;
+                      } else {
+                        return const Text('loading user...');
+                      }
+                    },
+                  ))
               .toList(),
         );
       },
     );
   }
 
-  Widget _buildUserListItem(DocumentSnapshot document) {
+  Future<Widget> _buildUserListItem(DocumentSnapshot document) async {
     Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
 
     if (_auth.currentUser!.email != data['email']) {
+      String imageUrl =
+          await _image_service.getPfpUrlId(data['uid'] ?? 'empty') ?? 'empty';
+      print(imageUrl);
+
       return ListTile(
+        leading: ClipOval(
+            child: Image.network(
+          imageUrl.toString(),
+          // "https://firebasestorage.googleapis.com/v0/b/chatapp-dfdd5.appspot.com/o/images%2Favatar-male.png?alt=media&token=24f15f9f-5eb4-4071-9b10-462a2b948353",
+          width: 50,
+          height: 50,
+        )),
         title: Text(data['email']),
-        onTap: () {
+        onTap: () async {
+          // print(imageUrl);
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => ChatPage(
                 receiverUserEmail: data['email'],
                 receiverUserID: data['uid'],
+                imageUrl: imageUrl,
               ),
             ),
           );
