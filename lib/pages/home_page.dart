80ede6cda412c26f72ae6ed4720b581c332e5a12
Benevito 535220222 <benevito.535220222@stu.untar.ac.id>
2023-12-03 15:53:32 +0700

    fix typo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -97,7 +97,6 @@ class _HomePageState extends State<HomePage> {
         )),
         title: Text(data['email']),
         onTap: () async {
-          // print(imageUrl);
           Navigator.push(
             context,
             MaterialPageRoute(
